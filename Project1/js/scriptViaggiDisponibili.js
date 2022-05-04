f5a853854b78f38d3ecc106d53a40bead8b3cc4f
DarioIEL <104058513+DarioIEL@users.noreply.github.com>
2022-05-04 13:54:37 +0200

    agg

---
+++ Project1/js/scriptViaggiDisponibili.js
@@ -0,0 +1,58 @@
+$(function(){
+
+
+    $.ajax({
+        url: 'http://localhost:3000/viaggi',
+        type: "GET",
+        dataType: 'json',
+        success: function(response){
+            console.log(response);
+            for(var i = 0; i < response.length; i++){
+                costruisciCard(response[i].id, response[i].nome, response[i].immagine, response[i].descrizione);
+            }
+        
+        }
+    });
+
+
+    function costruisciCard(id, nome, immagine, descrizione){
+        let card = `<div class="card col-3" data-id="${id}" >`+
+        `<img class="card-img-top" src="${immagine}" alt="Card image cap">`+  //Alt + 96 -> backtick
+        '<div class="card-body">'+
+          `<h5 class="card-title">${nome}</h5>`+
+          `<p class="card-text">${descrizione}</p>`+
+          `<a href="#" class="btn btn-primary" id="btnCompra">Go somewhere</a>`+
+        '</div>'+
+      '</div>';
+    
+    
+      
+      $(document).on('click', '#btnCompra', function(){
+          acquista(id, nome, immagine, descrizione);
+          console.log('ciao');
+      });
+          
+      $('#contCards').append(card);
+      
+    }
+    
+});
+
+
+function acquista(id, nome, immagine, descrizione){
+    $.ajax({
+        url: 'http://localhost:3000/viaggi/'+ id,
+        type: 'PUT',
+        dataType: 'json',
+        data: {
+            nome: nome,
+            immagine: immagine,
+            descrizione: descrizione,
+            acquistato: true
+        },
+        success: function(){
+            console.log('ok');
+        }
+
+    });
+}
