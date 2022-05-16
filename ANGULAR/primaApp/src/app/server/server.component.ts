926cff5c38f741b71a389172cfa3512463b177b0
DarioIEL <104058513+DarioIEL@users.noreply.github.com>
2022-05-16 10:07:38 +0200

    agg

--- ANGULAR/primaApp/src/app/server/server.component.ts
+++ ANGULAR/primaApp/src/app/server/server.component.ts
@@ -8,11 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class ServerComponent implements OnInit {
   server = 'FTP332 Serv';
   serverID = 12;
-  statusServer = 'online';
+  statusServer = '';
 
   constructor() { }
 
   ngOnInit(): void {
+    //voglio cambiare lo status del server in modo Random
+    //operatore ternario
+    // this.statusServer = Math.random() > 0.5 ? 'online' : 'offline';
+    let numRand = Math.random();
+    if(numRand > 0.5){
+      this.statusServer = 'online';
+    }else{
+      this.statusServer = 'offline';
+    }
+
   }
+
+
+  getServerStatus(){
+    return this.statusServer
+  }
+
+
 
 }
